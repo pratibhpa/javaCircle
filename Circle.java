07f86786b2a352942486221c8d340e331b2e3e5f
Pratibha Patel <you@example.com>
2019-03-22 01:21:27 +0530

    first java commit

---
+++ Circle.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float PI=3.14f,area;
+int radius=2;
+area=PI*radius*radius;
+System.out.println("Area of circle is:"+area);
+	}
+
+}
